bdbf7feb726da9723f637956878f75c34407821d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-04 14:34:27 +0100

    Add try-catch to fetchWeather function
    Add try-catch to fetchWeather function, and in case of error return the error and use alert message.

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -5,15 +5,21 @@ const API_KEY = "f33a484cf794d08d0148764789aaba32";
 
 //create function
 const fetchWeather = async (query) => {
-  const { data } = await axios.get(URL, {
-    params: {
-      q: query,
-      units: "metric",
-      APPID: API_KEY,
-    },
-  });
+  try {
+    const { data } = await axios.get(URL, {
+      params: {
+        q: query,
+        units: "metric",
+        APPID: API_KEY,
+      },
+    });
 
-  return data;
+    return data;
+  } catch (err) {
+    console.log("this is my error: " + err);
+    alert("Sorry, something went wrong, please try again!");
+    return err;
+  }
 };
 
 export default fetchWeather;
