b8109bb48a838b9d782e76484d98ae427fec56f5
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-04 10:37:34 +0100

    Update App() function
    Add <input/> element and use value, onChange and onKeyDown keywords. Use console.log to test if API call was successful.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import fetchWeather from "./api/fetchWeather";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
   //create state variable
   const [query, setQuery] = useState("");
 
@@ -12,8 +11,24 @@ function App() {
     //check if pressed key is equel to "enter"
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
+
+      //test if data is returned after API call
+      console.log(data);
     }
   };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Type your city for weather!"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={search}
+      />
+    </div>
+  );
 }
 
 export default App;
