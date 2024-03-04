f05ff47da3e2bc27fa67ed39862e99df4f84e438
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-04 10:34:53 +0100

    Add fetchWeather to App.js
    Import useState and fetchWeather into App.js file. Create state variable called "query" with empty string initial state. Create function and use if conditional statement to check if "enter" key is pressed. Use fetchWeather if conditional statement is true.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import fetchWeather from "./api/fetchWeather";
 import "./App.css";
 
 function App() {
   return <div className="App">Hello World!</div>;
+  //create state variable
+  const [query, setQuery] = useState("");
+
+  //create function
+  const search = async (e) => {
+    //check if pressed key is equel to "enter"
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+    }
+  };
 }
 
 export default App;
