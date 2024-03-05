3dad8314a882ee27156d71c5b816728de3b90dbd
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2024-03-05 12:03:38 +0100

    Updates

---
+++ serviceworker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+//to remove warning-message: "Unexpected use of 'self'."
+//"this" represents the serviceworker
+const self = this;
+
+// install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+//activate the SW
+//to keep only the needed cache version
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
